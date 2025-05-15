60065f3a919d08579c57e495436966053f155367
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-05-15 20:15:01 +0530

    first commit

---
+++ Task-Manager-Frontend-main/src/api/client.js
@@ -0,0 +1,8 @@
+export const AUTHURL =
+  "https://project-management-backend-blue.vercel.app/api/auth";
+export const PROJECTURL =
+  "https://project-management-backend-blue.vercel.app/api/project";
+export const TASKURL =
+  "https://project-management-backend-blue.vercel.app/api/task";
+export const ACTIVITYURL =
+  "https://project-management-backend-blue.vercel.app/api/activity";
