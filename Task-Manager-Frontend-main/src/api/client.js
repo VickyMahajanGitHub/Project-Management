d3b42e6759a95562a9c734dbee2dcbcdb662fa6c
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-05-20 11:29:26 +0530

    url-modified

--- Task-Manager-Frontend-main/src/api/client.js
+++ Task-Manager-Frontend-main/src/api/client.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://project-management-kappa-jet.vercel.app/";
+export const BASE_URL = "https://project-management-kappa-jet.vercel.app";
 
 export const AUTHURL =
   `${BASE_URL}/api/auth`;
